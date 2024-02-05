d7dc3ad89d2a9ff8c5f553c1005ca617bbf9fcc2
ItsArupSaha <sahaarup911@gmail.com>
2024-02-05 18:57:46 +0600

    social share button

---
+++ app.js
@@ -0,0 +1,17 @@
+const link = encodeURI('https://openjavascript.info');
+const msg = encodeURIComponent("Hi, everyone");
+const title = encodeURIComponent(document.querySelector('title').textContent);
+
+console.log([link, msg, title]);
+
+const whatsapp = document.querySelector('.wa-btn');
+whatsapp.href = `https://api.whatsapp.com/send?text=${msg}:${link}`;
+
+const fb = document.querySelector('.fb-btn');
+fb.href = `https://www.facebook.com/share.php?u=${link}`;
+
+const twitter = document.querySelector('.twitter-btn');
+twitter.href = `http://twitter.com/share?&url=${link}&text=${msg}&hashtags=javascript,programming`;
+
+const linkedIn = document.querySelector('.ln-btn');
+linkedIn.href = `https://www.linkedin.com/sharing/share-offsite/?url=${link}`;
